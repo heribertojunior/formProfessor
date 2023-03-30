6b0e0bf3fc13c097cb26ac4540dd8a14f2d5421a
HeribertoJunior <hsc.jr94@gmail.com>
2023-03-30 08:22:10 -0300

    upload do projeto

---
+++ app/views.py
@@ -0,0 +1,134 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
+from app.models import Professor
+from app.models import Coordenadoria
+from app.forms import ProfessorForm
+from django.urls import reverse
+from app.models import Bimestre
+from app.forms import BimestreForm
+from app.models import Pergunta
+from app.forms import PerguntaForm
+from app.models import Resposta
+from app.forms import RespostaForm
+from app.models import Escola, Turno , Serie, Turma, Materia
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class ModeloView(TemplateView):
+    template_name = "model.html"
+
+class SobreView(TemplateView):
+    template_name = "sobre.html"
+
+class MatView(TemplateView):
+    template_name = "questoes.html"
+
+class PortView(TemplateView):
+    template_name = "portugues.html"
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'index.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user =authenticate(username=username, password=senha)
+
+        if user:
+            return render(request, 'login.html')
+        else:
+            return render(request, 'negado.html')
+
+@login_required
+def plataforma(request):
+    if request.user.is_authenticated:
+        return render(request, 'login.html')
+    else:
+        return render('negado.html')
+
+def index(request):
+    if request.method == "GET":
+        form_professor = ProfessorForm()
+
+        context = {
+            'form_professor': form_professor,
+    }
+        return render(request, "form.html", context = context)
+
+    elif request.method == "POST":
+        print("passou no Elif")
+        form_professor = ProfessorForm(request.POST)
+        if form_professor.is_valid():
+            print("chegou na validação")
+           # escolas = Escola.objects.filter(id__in=request.POST.getlist('escolas'))
+            form_professor.save()
+          # f_cont = form_mat_perg.save(commit=False)
+          # f_cont.professor = Professor.objects.get(id=form_professor.instance.id)
+          # f_cont.materia = Materias.objects.get(id=int(request.POST.get('materia')))
+          # f_cont.save()
+
+            return render (request, "questoes.html")
+
+        else:
+
+            print("Nao validou.")
+            return render (request, "index.html")
+        if request.POST.get('componente_curricular') == 'Matematica':
+            print (request.POST.get('componente_curricular'))
+            return render (request, "questoes.html")
+        elif request.POST.get('componente_curricular') == 'Portugues':
+            print (request.POST.get('componente_curricular'))
+            return render (request, "portugues.html")
+
+    else:
+            context = {
+            'form_professor': form_professor,
+            }
+
+            return render (request, "form.html", context)
+
+def getCoordenadorias(request):
+    data = json.loads(request.body)
+    jurisdicao_id = data["id"]
+    coordenadorias = Coordenadoria.objects.filter(jurisdicao_id=jurisdicao_id)
+    return JsonResponse(list(coordenadorias.values("id", "coordenadoria")), safe=False)
+
+def getEscolas(request):
+    data = json.loads(request.body)
+    coordenadoria_id = data["id"]
+    escolas = Escola.objects.filter(coordenadoria_id=coordenadoria_id)
+    return JsonResponse(list(escolas.values("id", "escola")), safe=False)
+
+
+def getTurnos(request):
+    data = json.loads(request.body)
+    escola_id = data["id"]
+    turnos = Turno.objects.filter(escola_id=escola_id)
+    return JsonResponse(list(turnos.values("id", "turno")), safe=False)
+
+def getSeries(request):
+    data = json.loads(request.body)
+    turno_id = data["id"]
+    series = Serie.objects.filter(turno_id=turno_id)
+    return JsonResponse(list(series.values("id", "serie")), safe=False)
+
+def getTurmas(request):
+    data = json.loads(request.body)
+    serie_id = data["id"]
+    turmas = Turma.objects.filter(serie_id=serie_id)
+    return JsonResponse(list(turmas.values("id", "turma")), safe=False)
+
+def getMaterias(request):
+    data = json.loads(request.body)
+    serie_id = data["id"]
+    materias = Materia.objects.filter(serie_id=serie_id)
+    return JsonResponse(list(materias.values("id", "materia")), safe=False)
